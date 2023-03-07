d88f6394eb29c0a52acbecb2ef69909e8918ccef
scotjlefg <118236166+scotjlefg@users.noreply.github.com>
2023-03-07 01:03:15 -0500

    20230307 0105 Added user management functions in user app views

--- users/views.py
+++ users/views.py
@@ -32,11 +32,48 @@ def user_registration(request):
     else:
         return HttpResponse('user registration Not Saved')
     
+
 #end def user_registration
 
 # Changing Passwords
 
+def change_password(request):
+    username = request.POST['username']
+    new_password = request.POST['new_password']
+    user = User.objects.get(username=username)
+    user.set_password(new_password)
+    user.save()
+    return HttpResponse('User reset password')
+#end def change_password
+
+
+
 # Authentication, Login & Logout
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            userModeObject = User.objects.get(username=request.user)
+            context = {'userInfo':userModelObject}
+
+            return HttpResponse('user login successful')
+        else:
+            # Return to an 'invalid login' error message
+            return HttpResponse("Login creditials did not match")
+    return HttpResponse('Direct to Login page (home page?)')
+
+
+def user_logout(request):
+    logout(request)
+    # redirect to a home page
+    return HttpResponse('logout - redirect to homepage')
+#end def user_logout
+
 
 # Authorization
 
