e1233286569a1d00ab474f30726dc1c0e428af28
scotjlefg <118236166+scotjlefg@users.noreply.github.com>
2023-03-18 23:01:23 -0400

    20230318 2300 Links are tested and working

--- products/views.py
+++ products/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
-from .models import ProductItem
+from .models import ProductItem 
+from .models import UploadImage
 
 # Create your views here.
 
