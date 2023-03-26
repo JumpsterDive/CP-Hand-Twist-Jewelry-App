09509e230154c29186bc65dd00def0c27e41d108
scotjlefg <118236166+scotjlefg@users.noreply.github.com>
2023-03-25 23:15:48 -0400

    20230325 2315 Working detail product page links

--- products/urls.py
+++ products/urls.py
@@ -5,7 +5,7 @@ app_name = 'products'
 
 urlpatterns = [
     path('allproducts/', views.product_all, name ='product_all'),
-    path('productDetails/<int:id>/',views.productDetailsIndex, name='productDetailsIndex'),
+    # path('productDetails/<int:id>/',views.productDetailsIndex, name='productDetailsIndex'),
     path('productDetail/<slug:slug>/', views.productDetail, name = 'productDetail'),
     path('search/<slug:category_slug>/',views.category_list , name='category_list')
 
--- products/views.py
+++ products/views.py
@@ -10,6 +10,7 @@ from django.views.generic import View
 
 def product_all(request):
     all_products = ProductItem.products.all()
+    print("Product_all")
     return render(request, "products/products.html", {'all_products':all_products,})
 #end def product_all
 
@@ -25,74 +26,6 @@ def category_list(request, category_slug=None):
     return render(request, 'products/category.html', {'category': category, 'products': products})
 
 
-def productIndexPage1(request,category_slug=None):
-
-    products = ProductItem.products.all()
-
-    print(category_slug)
-    category = get_object_or_404(Category,slug=category_slug)
-
-    print(products)
-
-    # User.objects.get(username=request.user)
-    # product = ProductItem.objects.get(product_type)
-    # product = ProductItem(self.product_type)
-    # print(product)
-    # category = Category.objects.all()
-    # product_types = ProductType.objects.get(id=id)
-    # all_products = ProductItem.objects.filter(ProductItem.product_type)
-
-    # all_products = ProductItem.objects.filter(product_type='Studs').first()
-    # all_products = ProductItem.objects.filter().product_type('Cuffs')
-    all_products = ProductType.objects.all()
-
-    # queryset = ProductType.objects.all()
-    # if self.request.GET.get('Cuffs'):
-    #     queryset = queryset.filter(product_type=self.requessss.)
-
-
-    print(all_products)
-    # Cuffs
-    # Studs
-    # Gemstones
-    # Ear Climbers
-    
-
-    # print(request)
-    # print(category)
-    # print(product_types)
-    print(all_products)
-    
-    
-    
-    return render(request, "products/products.html", {
-                                    'all_products':all_products,
-                                     })
-
-def productPage2_index(request):
-    all_products = ProductItem.objects.all()
-    return render(request, "products/products.html", {
-                                    'all_products':all_products,
-                                     })
-
-def productPage3_index(request):
-    all_products = ProductItem.objects.all()
-    return render(request, "products/products.html", {
-                                    'all_products':all_products,
-                                     })
-
-def productDetailsIndex(request,id):
-    # all_products = ProductItem.objects.all()
-    # productDetailsObject = ProductItem.objects.filter(id=id)
-    productDetailsObject = ProductItem.objects.get(id=id)
-    context = {'productDetail': productDetailsObject}
-    print(context)
-    
-    return render(request, "products/productDetails.html", context)
-
-#end def productDetails_index
-
-
 def productDetail(request,slug):
 
     product = get_object_or_404(ProductItem, slug=slug)
@@ -101,10 +34,78 @@ def productDetail(request,slug):
     context = {'productDetail': product}
     print(context)
     print("HERE")
-    return render(request,'products/productsDetails.html',context)
+    return render(request,'products/productDetails.html',context)
     # all_products = ProductItem.objects.all()
     # productDetailsObject = ProductItem.objects.filter(id=id)
     # return render(request, "products/productDetails.html", context)
  
 
 #end def productDetails_index
+
+
+# def productDetailsIndex(request,id):
+#     # all_products = ProductItem.objects.all()
+#     # productDetailsObject = ProductItem.objects.filter(id=id)
+#     productDetailsObject = ProductItem.objects.get(id=id)
+#     context = {'productDetail': productDetailsObject}
+#     print(context)
+    
+#     return render(request, "products/productDetails.html", context)
+
+# #end def productDetails_index
+
+# def productIndexPage1(request,category_slug=None):
+
+#     products = ProductItem.products.all()
+
+#     print(category_slug)
+#     category = get_object_or_404(Category,slug=category_slug)
+
+#     print(products)
+
+#     # User.objects.get(username=request.user)
+#     # product = ProductItem.objects.get(product_type)
+#     # product = ProductItem(self.product_type)
+#     # print(product)
+#     # category = Category.objects.all()
+#     # product_types = ProductType.objects.get(id=id)
+#     # all_products = ProductItem.objects.filter(ProductItem.product_type)
+
+#     # all_products = ProductItem.objects.filter(product_type='Studs').first()
+#     # all_products = ProductItem.objects.filter().product_type('Cuffs')
+#     all_products = ProductType.objects.all()
+
+#     # queryset = ProductType.objects.all()
+#     # if self.request.GET.get('Cuffs'):
+#     #     queryset = queryset.filter(product_type=self.requessss.)
+
+
+#     print(all_products)
+#     # Cuffs
+#     # Studs
+#     # Gemstones
+#     # Ear Climbers
+    
+
+#     # print(request)
+#     # print(category)
+#     # print(product_types)
+#     print(all_products)
+    
+    
+    
+#     return render(request, "products/products.html", {
+#                                     'all_products':all_products,
+#                                      })
+
+# def productPage2_index(request):
+#     all_products = ProductItem.objects.all()
+#     return render(request, "products/products.html", {
+#                                     'all_products':all_products,
+#                                      })
+
+# def productPage3_index(request):
+#     all_products = ProductItem.objects.all()
+#     return render(request, "products/products.html", {
+#                                     'all_products':all_products,
+#                                      })
